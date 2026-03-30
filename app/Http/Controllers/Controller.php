5229d3656c75ca64ad22f53173a384f5a436cbe8
minhthu612 <hmt06122005@gmail.com>
2026-03-30 19:09:40 +0700

    GỐC

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class Controller
+{
+    function tt()
+    {
+        return "Trần Thị Thanh Tiền";
+    }
+
+    function ZanThi()
+    {
+        return "Đào Đặng Thuỳ Vân";
+    }
+
+    function HMT()
+    {
+        return "Huỳnh Minh Thư";
+    }
+
+    function VanCongThien()
+    {
+        return "Văn Công Thiện";
+    }
+
+    function tlda()
+    {
+        return "Trần Lê Đông Anh";
+    }
+
+    function hnt()
+    {
+        return "Hồ Ngọc Tiền";
+    }
+}
