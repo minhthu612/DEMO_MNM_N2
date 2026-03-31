69e80e4576bd6bd01c2bc9996c7176091aa6448f
username <0303239230245@st.buh.edu.vn>
2026-03-31 09:47:24 +0700

    tttt

--- resources/views/components/book-layout.blade.php
+++ resources/views/components/book-layout.blade.php
@@ -14,18 +14,18 @@
                     <nav class="navbar navbar-light">
                         <ul class="navbar-nav">
                             <li class="nav-item active">
-                            <a class="nav-link" href="{{url('sach')}}">Trang chủ</a>
+                            <a class="nav-link menu-the-loai" href="#" the_loai="">Trang chủ</a>
                             </li>
                             <li class="nav-item">
-                            <a class="nav-link" href="{{url('sach/theloai/1')}}">Tiểu thuyết</a>
+                            <a class="nav-link menu-the-loai" href="#" the_loai="1">Tiểu thuyết</a>
                             </li>
                             <li class="nav-item">
-                            <a class="nav-link" href="{{url('sach/theloai/2')}}">Truyện ngắn - tản văn</a>
+                            <a class="nav-link menu-the-loai" href="#" the_loai="2">Truyện ngắn - tản văn</a>
                             </li>
                             <li class="nav-item">
-                            <a class="nav-link" href="{{url('sach/theloai/3')}}">Tác phẩm kinh điển</a>
+                            <a class="nav-link menu-the-loai" href="#" the_loai="3">Tác phẩm kinh điển</a>
                             </li>
-                        </ul>
+                            </ul>
                     </nav>
                     <img src="{{asset('images/sidebar_1.jpg')}}" width="100%" class='mt-1'>
                     <img src="{{asset('images/sidebar_2.jpg')}}" width="100%" class='mt-1'>
